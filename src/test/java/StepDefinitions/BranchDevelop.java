485e388b08d0bcfc4679b5003de8566dbb36c3a2
Kajal <shevatekajal123@gmail.com>
2021-04-01 15:51:55 +0530

    added branch class

---
+++ src/test/java/StepDefinitions/BranchDevelop.java
@@ -0,0 +1,19 @@
+package StepDefinitions;
+
+import org.testng.annotations.Test;
+
+public class BranchDevelop {
+
+	
+	@Test
+	public String display()
+	{
+		return "Hello";
+	}
+	
+	public String draw()
+	{
+		return "Draw";
+	}
+	
+}
